4b28e6f26cb462aa0af2fc0dbf65f6f70d81a371
Zasetsky <zasetsky.evgeny@gmail.com>
2021-12-18 19:08:46 +0300

    first commit

---
+++ src/components/Sapper.vue
@@ -0,0 +1,30 @@
+<template>
+ <div class="field">
+ <div></div>
+ <div></div>
+ <div></div>
+ <div></div>
+ <div></div>
+ <div></div>
+ <div></div>
+ <div></div>
+ <div></div>
+ </div>
+</template>
+
+<script>
+import cells from './data/cell';
+export default {
+  name: 'Sapper',
+  data() {
+    return {
+      cells,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
