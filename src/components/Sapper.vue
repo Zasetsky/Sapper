1830e6c03660cc3302c489d433d9cf23036c0c24
Zasetsky <zasetsky.evgeny@gmail.com>
2022-01-08 19:11:33 +0300

    create bomb show after game

--- src/components/Sapper.vue
+++ src/components/Sapper.vue
@@ -91,7 +91,7 @@ export default {
 
     getFlag(itemX, itemY) {
       const cell = this.items[itemY][itemX];
-      if (!cell.isActive && !this.winMsg && !this.loseMsg) {
+      if (!cell.isActive && (!this.winMsg || !this.loseMsg)) {
         cell.flag = !cell.flag;
       }
     },
@@ -107,19 +107,26 @@ export default {
       let bombsAround = 0;
       if (this.items[y - 1] && this.items[y - 1][x] && this.items[y - 1][x].bomb) {
         bombsAround++;
-      } if (this.items[y - 1] && this.items[y - 1][x - 1] && this.items[y - 1][x - 1].bomb) {
+      }
+      if (this.items[y - 1] && this.items[y - 1][x - 1] && this.items[y - 1][x - 1].bomb) {
         bombsAround++;
-      } if (this.items[y][x - 1] && this.items[y][x - 1].bomb) {
+      }
+      if (this.items[y][x - 1] && this.items[y][x - 1].bomb) {
         bombsAround++;
-      } if (this.items[y + 1] && this.items[y + 1][x - 1] && this.items[y + 1][x - 1].bomb) {
+      }
+      if (this.items[y + 1] && this.items[y + 1][x - 1] && this.items[y + 1][x - 1].bomb) {
         bombsAround++;
-      } if (this.items[y + 1] && this.items[y + 1][x] && this.items[y + 1][x].bomb) {
+      }
+      if (this.items[y + 1] && this.items[y + 1][x] && this.items[y + 1][x].bomb) {
         bombsAround++;
-      } if (this.items[y + 1] && this.items[y + 1][x + 1] && this.items[y + 1][x + 1].bomb) {
+      }
+      if (this.items[y + 1] && this.items[y + 1][x + 1] && this.items[y + 1][x + 1].bomb) {
         bombsAround++;
-      } if (this.items[y][x + 1] && this.items[y][x + 1].bomb) {
+      }
+      if (this.items[y][x + 1] && this.items[y][x + 1].bomb) {
         bombsAround++;
-      } if (this.items[y - 1] && this.items[y - 1][x + 1] && this.items[y - 1][x + 1].bomb) {
+      }
+      if (this.items[y - 1] && this.items[y - 1][x + 1] && this.items[y - 1][x + 1].bomb) {
         bombsAround++;
       }
       this.items[y][x].bombsCountAround = bombsAround;
@@ -137,10 +144,12 @@ export default {
       if (cell.isActive) {
         classes.push('active');
         if (cell.bomb) {
-          classes.push('bomb');
+          classes.push('bombClicked');
         } else {
           classes.push(this.getBombsCountClass(cell));
         }
+      } else if (cell.bomb && (this.winMsg || this.loseMsg)) {
+        classes.push('bomb');
       } else if (cell.flag) {
         classes.push('flag');
       }
@@ -201,8 +210,11 @@ export default {
 .grid8 {
   background: url('../data/images/grid8.png') no-repeat;
 }
-.bomb {
+.bombClicked {
   background: url('../data/images/mineClicked.png') no-repeat;
+}
+.bomb {
+  background: url('../data/images/mine.png') no-repeat;
 }
 .start-btn {
   width: 60px;
